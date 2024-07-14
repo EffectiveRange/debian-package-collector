ca18d2af6c338b6abdcef5b42e40e78434dc1be6
GitHub Action <action@github.com>
2024-07-14 21:18:00 +0000

    Update version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='debian-package-collector',
-    version='0.1.0',
+    version='1.0.0',
     description='Debian package collector to download .deb packages from new releases',
     author='Ferenc Nandor Janky & Attila Gombos',
     author_email='info@effective-range.com',
